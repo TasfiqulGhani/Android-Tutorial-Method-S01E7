8894216ee69416a6e7091170b5a6aa3ffb11c362
TasfiqulGhani <tasfiqulghani@gmail.com>
2017-01-04 22:43:16 +0600

    codes

---
+++ src/main.java
@@ -0,0 +1,28 @@
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Sub(10,5);
+		 
+
+	}
+	
+	
+	public static void Add(int numberone,int numbertwo){
+		 
+		int result=numberone+numbertwo;
+		System.out.print(result);
+		
+	}
+	
+	public static void Sub(int numberone,int numbertwo){
+		
+		System.out.print(numberone-numbertwo);
+	}
+
+}
